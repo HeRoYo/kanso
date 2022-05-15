09d96e7631be67586b6343e96e9c75293e794e2e
HeRoYo <11bykko11@gmail.com>
2022-05-15 19:39:12 +0900

    マイグレーションの調整

--- db/migrate/20220515102035_create_posts.rb
+++ db/migrate/20220515102035_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string  :title
-      t.integer :genre_id
-      t.text    :text
+      t.string  :title    , null: false
+      t.integer :genre_id , null: false
+      t.text    :text     , null: false
 
       t.timestamps
     end
