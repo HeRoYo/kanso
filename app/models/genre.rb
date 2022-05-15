4d2d3d4bca40094817f471663846b9fa9aeea86e
HeRoYo <11bykko11@gmail.com>
2022-05-15 19:38:10 +0900

    ジャンルモデルの作成

---
+++ app/models/genre.rb
@@ -0,0 +1,12 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '本・マンガ' },
+    { id: 3, name: '本・小説' },
+    { id: 4, name: '本・その他' },
+    { id: 5, name: '映像・映画' },
+    { id: 6, name: '映像・アニメ' },
+    { id: 7, name: '映像・ドラマ' },
+    { id: 8, name: '映像・その他' }
+  ]
+  end
