d28b2ed0d5ddbc1f57236afc14adee2acaeeb67e
HeRoYo <11bykko11@gmail.com>
2022-05-22 04:21:58 +0900

    モデルの単体テストの準備

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe '感想投稿' do
+    context '感想が投稿ができる場合' do
+      it 'タイトル、ジャンル、内容が存在すると投稿できる' do
+        
+      end
+    end
+    context '感想が投稿できない場合' do
+      it 'タイトルが空では投稿できない' do
+        
+      end
+      it 'タイトルが21文字以上だと投稿できない' do
+        
+      end
+      it 'ジャンルが空だと投稿できない' do
+        
+      end
+      it '内容が空だと投稿できない' do
+        
+      end
+      it '内容が10001文字以上だと投稿できない' do
+        
+      end
+    end
+  end
+end
