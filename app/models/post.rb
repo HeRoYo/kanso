5d8e8aa28aa16002aa393b978d2973692f6d53f5
HeRoYo <11bykko11@gmail.com>
2022-05-22 06:18:29 +0900

    感想機能のモデルの単体テスト

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
 
   with_options presence: true do
     validates :title, length: { maximum: 20 } # 20文字以下
-    validates :text, length: { maximum: 10000 } #１万文字以下
+    validates :text, length: { maximum: 10_000 } #１万文字以下
   end
 
   validates :genre_id, numericality: { other_than: 1, message: "can't be blank"} 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,18 +8,39 @@ RSpec.describe Post, type: :model do
   describe '感想投稿' do
     context '感想が投稿ができる場合' do
       it 'タイトル、ジャンル、内容が存在すると投稿できる' do
+        expect(@post).to be_valid
       end
     end
     context '感想が投稿できない場合' do
       it 'タイトルが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Title can't be blank"
       end
       it 'タイトルが21文字以上だと投稿できない' do
+        @post.title = Faker::Lorem.characters(number: 21)
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Title is too long (maximum is 20 characters)"
       end
       it 'ジャンルが空だと投稿できない' do
+        @post.genre_id = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Genre can't be blank"
+      end
+      it 'ジャンルに１を選択していると投稿できない' do
+        @post.genre_id = 1
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Genre can't be blank"
       end
       it '内容が空だと投稿できない' do
+        @post.text = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Text can't be blank"
       end
       it '内容が10001文字以上だと投稿できない' do
+        @post.text = Faker::Lorem.characters(number: 10_001)
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Text is too long (maximum is 10000 characters)"
       end
     end
   end
