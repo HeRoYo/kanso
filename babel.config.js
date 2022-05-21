8aad097ec8a518cfb5dfdd33e339a5b77cc1cdbc
HeRoYo <11bykko11@gmail.com>
2022-05-22 06:39:09 +0900

    heroku error

--- babel.config.js
+++ babel.config.js
@@ -66,7 +66,13 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      [
+        "@babel/plugin-proposal-private-property-in-object", 
+        {
+           "loose": true 
+          }
+        ]
     ].filter(Boolean)
   }
 }
