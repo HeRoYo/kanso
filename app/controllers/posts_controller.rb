d04f56a66ca5b2d4442346bd391244eb3f591e3c
HeRoYo <11bykko11@gmail.com>
2022-05-28 02:52:22 +0900

    ログインしてないユーザーは投稿できない処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
   def index
     @posts = Post.order("created_at DESC")
   end
@@ -20,5 +22,11 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :genre_id, :text).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
